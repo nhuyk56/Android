59b615e6ba2c6f3d7caa6b9cc9730c39d1c7e559
nhuyk56@gmail.com <nhuyk56@gmail.com>
2017-05-06 21:02:26 +0700

    Bài 11 và Bài 12 Time: 20h55, Date: 06/05/2017

--- BaiTap12ViDuTongHopTextViewEditTextCheckBoxButtonVaImageButtonTrongAndroidNguyenNhuY5613177856th2/gen/com/example/NguyenNhuY_56th2_56131778_Bai12_Java/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.NguyenNhuY_56th2_56131778_Bai12_Java;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- baiTap11SuDungCheckboxVaRadioButtonTrongAndroidNguyenNhuY56131778/gen/com/example/baitap11sudungcheckboxvaradiobuttontrongandroidnguyennhuy_56131778/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.baitap11sudungcheckboxvaradiobuttontrongandroidnguyennhuy_56131778;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
