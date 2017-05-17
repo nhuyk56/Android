0de5d565fdd08295885f01f9f627b3b66046d3d1
nhuyk56@gmail.com <nhuyk56@gmail.com>
2017-05-17 21:25:33 +0700

    Bài tập 14: Thực hành về Custom Layout cho ListView trong Android

---
+++ BaiTap14_Custom_Layout_ListView_NguyenNhuY_56131778_56th2/gen/com/example/baitap14_custom_layout_listview_nguyennhuy_56131778_56th2/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.baitap14_custom_layout_listview_nguyennhuy_56131778_56th2;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ BaiTap14_Custom_Layout_ListView_NguyenNhuY_56131778_56th2/src/com/example/baitap14_custom_layout_listview_nguyennhuy_56131778_56th2/Employee.java
@@ -0,0 +1,29 @@
+package com.example.baitap14_custom_layout_listview_nguyennhuy_56131778_56th2;
+
+public class Employee {
+ private String id;
+ private String name;
+ private boolean gender;
+ public String getId() {
+ return id;
+ }
+ public void setId(String id) {
+ this.id = id;
+ }
+ public String getName() {
+ return name;
+ }
+ public void setName(String name) {
+ this.name = name;
+ }
+ public boolean isGender() {
+ return gender;
+ }
+ public void setGender(boolean gender) {
+ this.gender = gender;
+ }
+ @Override
+ public String toString() {
+ return this.id+"-"+this.name;
+ }
+}
