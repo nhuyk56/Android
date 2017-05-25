20094fe45f30cd359cdbf837de23aee278cfca3d
nhuyk56@gmail.com <nhuyk56@gmail.com>
2017-05-25 21:11:19 +0700

    Xóa

--- Bai_28_1_InternalStorage_Nhu_Y_56131778/gen/com/example/bai_28_1_internalstorage_nhu_y_56131778/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.bai_28_1_internalstorage_nhu_y_56131778;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- Bai_28_2_ExternalStorage_Nhu_Y_56131778/gen/com/example/bai_28_2_externalstorage_nhu_y_56131778/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.bai_28_2_externalstorage_nhu_y_56131778;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
