04165b8c225525ee9239aefab9d244170cbaee78
nhuyk56@gmail.com <nhuyk56@gmail.com>
2017-05-25 21:09:31 +0700

    Bài tập 28: Xử lý tập tin trong Android

---
+++ Bai_28_3_Savingcachefiles_Nhu_Y_56131778/gen/com/example/bai_28_3_savingcachefiles_nhu_y_56131778/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.bai_28_3_savingcachefiles_nhu_y_56131778;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
